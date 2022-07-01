32c8dd29fc2983a99dae9c45de4b511618dd45e6
PabloPerezMedel <pabloperezmedel@hotmail.com>
2022-07-01 18:23:09 -0500

    envio de correos

--- correos.php
+++ correos.php
@@ -1,18 +1,22 @@
 <?php 
-    $destinatario = 'pabloperezmedel@hotmail.com';  //Este es el correo al que se enviaran los mensajes del formulario
+     if(isset$_POST['enviar']){
+        $destinatario = 'pabloperezmedel@hotmail.com';  //Este es el correo al que se enviaran los mensajes del formulario
 
-    $nombre = $_POST['nombre'];
-    $telefono = $_POST['telefono'];
-    $email = $_POST['email'];
-    $regimenfiscal = $_POST['regimenfiscal'];
-    $serviciosinteres = $_POST['serviciosinteres'];
-    $mensaje = $_POST['mensaje'];
+        $nombre = $_POST['nombre'];
+        $telefono = $_POST['telefono'];
+        $email = $_POST['email'];
+        $regimenfiscal = $_POST['regimenfiscal'];
+        $serviciosinteres = $_POST['serviciosinteres'];
+        $mensaje = $_POST['mensaje'];
+    
+        $header = "Enviado desde la página del Despacho dp";
+        $mensajeCompleto = $mensaje . "\n Maneras de contactarme: " . $telefono " o tambien por correo el cual es: " . $email " \n tentamente: " . $nombre;
+    
+        $mail = @mail($destinatario, $serviciosinteres, $regimenfiscal, $mensajeCompleto, $header);
 
-
-    $header = "Enviado desde la página del Despacho dp";
-    $mensajeCompleto = $mensaje . "\n Maneras de contactarme: " . $telefono " o tambien por correo el cual es: " . $email " \n tentamente: " . $nombre;
-
-    mail($destinatario, $serviciosinteres, $regimenfiscal, $mensajeCompleto, $header);
-    echo "<script>alert('El correo fue enviado exitosamente')</script>";
-    echo "<script>setTimeout(\"location.href='index.html'\", 1000)</script>";
+        if($mail){
+            echo "<script>alert('El correo fue enviado exitosamente')</script>";
+            echo "<script>setTimeout(\"location.href='index.html'\", 1000)</script>";
+        }
+     }
 ?>
