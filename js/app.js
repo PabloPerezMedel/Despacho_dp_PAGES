59688edb80f5f53a34f81c912b1f146f9a098269
PabloPerezMedel <pabloperezmedel@hotmail.com>
2022-07-04 22:11:16 -0500

    Correcciones

---
+++ js/app.js
@@ -0,0 +1,91 @@
+// Navegation Menu
+let btnMenu = document.querySelector('.btn-menu');
+let barIconX = document.querySelector('.btn-menu i');
+let menu = document.querySelector('.list-container');
+let menuContent = document.querySelector('.menu');
+var activador = true;
+
+btnMenu.addEventListener('click', (event) => {
+
+  //Icon X
+  barIconX.classList.toggle('fa-times');
+
+   if(activador){
+     menu.style.left = '0%'; 
+     menu.style.transition = '0.5s';
+  
+     activador = false;
+   }
+   else{
+    activador = false;
+    menu.style.left = '-100%';
+
+    activador = true;
+   }
+
+});
+
+// Intercalar la clase active
+let enlaces = document.querySelectorAll('.lists li a');
+
+enlaces.forEach((element) => {
+   
+  element.addEventListener('click', (event) => {
+   enlaces.forEach((link) => {
+     link.classList.remove('active');
+   });
+    event.target.classList.add('active');
+  });
+});
+
+//Efecto Scroll
+
+ let prevScrollPos = window.pageYOffset;
+ let goTop = document.querySelector('.go-top');
+
+window.onscroll = () => {
+  
+  //Mostrar y Ocultar Menu - Scroll Menu (Function)
+  let currentScrollPos = window.pageYOffset;
+
+  if (prevScrollPos > currentScrollPos) {
+    menuContent.style.top = '0px';
+    menuContent.style.transition = '0.5s';
+  }else{
+    menuContent.style.top = '-60px';
+    menuContent.style.transition = '0.5s';
+  }
+  prevScrollPos = currentScrollPos;
+  
+  //Mostrar y Ocultar Scoll Estilos
+  let arriba = window.pageYOffset;
+
+  //Conditions
+  if(arriba <= 600){
+    menuContent.style.borderBottom = 'none';
+
+    //Ocultar Go Top
+    goTop.style.right = '-100px';
+  }else{
+    menuContent.style.borderBottom = '3px solid #2C2F40';
+
+    //Mostrar Go Top
+    goTop.style.right = '0px';
+    goTop.style.transition = '0.5s';
+  }
+  
+} 
+
+//Go Top Click
+goTop.addEventListener('click', () => {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+});
+
+let abajo = document.querySelector('#abajo');
+
+abajo.addEventListener('click', () => {
+  document.body.scrollTop = 715;
+  document.documentElement.scrollTop = 715;
+  
+});
